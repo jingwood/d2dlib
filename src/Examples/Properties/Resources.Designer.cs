0ed252e6361ad6eaf146cd585109c934f97ac0ff
Jing <dujid0@gmail.com>
2019-07-19 16:16:34 +0900

    add transparent icons draw example

--- src/Examples/Properties/Resources.Designer.cs
+++ src/Examples/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace unvell.D2DLib.Examples.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,56 @@ namespace unvell.D2DLib.Examples.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_car_production_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_car_production_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_himeji_castle_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_himeji_castle_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_home_page_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_home_page_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_music_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_music_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_uwu_emoji_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_uwu_emoji_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
