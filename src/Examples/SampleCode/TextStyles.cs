4fb8e12ae21b8a17065006cb08efed9979f8666d
Jingwood <dujid0@gmail.com>
2022-10-28 21:12:39 +0900

    add text styles samples

--- src/Examples/SampleCode/TextStyles.cs
+++ src/Examples/SampleCode/TextStyles.cs
@@ -40,6 +40,20 @@ namespace unvell.D2DLib.Examples.SampleCode
 			BackColor = Color.White;
 		}
 
+		static readonly D2DFontStyle[] SampleFontStyles = new D2DFontStyle[]{
+			 D2DFontStyle.Normal, D2DFontStyle.Oblique, D2DFontStyle.Italic
+		};
+
+		static readonly D2DFontWeight[] SampleFontWeights = new D2DFontWeight[]{
+			 D2DFontWeight.Normal, D2DFontWeight.Medium,
+			 D2DFontWeight.SemiLight, D2DFontWeight.Light, D2DFontWeight.UltraLight,
+			 D2DFontWeight.SemiBold, D2DFontWeight.Bold, D2DFontWeight.UltraBold,
+		};
+
+		static readonly D2DFontStretch[] SampleFontStretch = new D2DFontStretch[]{
+			 D2DFontStretch.Normal, D2DFontStretch.Medium, D2DFontStretch.Condensed,
+		};
+
 		protected override void OnRender(D2DGraphics g)
 		{
 			base.OnRender(g);
@@ -47,19 +61,19 @@ namespace unvell.D2DLib.Examples.SampleCode
 			var rect = new D2DRect(100, 20, 400, 20);
 
 			int i = 0;
-			foreach (var fontStyle in Enum.GetValues(typeof(D2DFontStyle)))
+			foreach (var fontStyle in SampleFontStyles)
 			{
 				var styleKeyName = Enum.GetName(typeof(D2DFontStyle), fontStyle);
 
-				foreach (var fontStretch in Enum.GetValues(typeof(D2DFontStretch)))
+				foreach (var fontStretch in SampleFontStretch)
 				{
 					var stretchKeyName = Enum.GetName(typeof(D2DFontStretch), fontStretch);
 
-					foreach (var fontWeight in Enum.GetValues(typeof(D2DFontWeight)))
+					foreach (var fontWeight in SampleFontWeights)
 					{
 						var weightKeyName = Enum.GetName(typeof(D2DFontWeight), fontWeight);
 
-						g.DrawText((i++).ToString(), D2DColor.Black, "Arial", 10,
+						g.DrawText((++i).ToString(), D2DColor.Black, "Arial", 10,
 							new D2DRect(rect.X, rect.Y-10, 20, 20));
 
 						g.DrawText($"{styleKeyName}, {stretchKeyName}, {weightKeyName}", D2DColor.Black, "Arial", 16,
@@ -71,7 +85,7 @@ namespace unvell.D2DLib.Examples.SampleCode
 						if (rect.X > ClientSize.Width - 100)
 						{
 							rect.X = 100;
-							rect.Y += 30;
+							rect.Y += 38;
 						}
 					}
 				}
