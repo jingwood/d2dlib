ecaea22367124e51ecdfe0a2276f54c67515309f
Jingwood <dujid0@gmail.com>
2023-01-02 20:35:25 +0900

    move enum define to D2DEnum.cs, add comments

--- src/D2DLibExport/D2DEnums.cs
+++ src/D2DLibExport/D2DEnums.cs
@@ -601,5 +601,36 @@ namespace unvell.D2DLib
 		/// </summary>
 		UltraExpanded = 9
 	};
+
+	/// <summary>
+	/// This enumeration describes the type of combine operation to be performed.
+	/// </summary>
+	public enum D2D1_COMBINE_MODE
+	{
+
+		/// <summary>
+		/// Produce a geometry representing the set of points contained in either the first
+		/// or the second geometry.
+		/// </summary>
+		D2D1_COMBINE_MODE_UNION = 0,
+
+		/// <summary>
+		/// Produce a geometry representing the set of points common to the first and the
+		/// second geometries.
+		/// </summary>
+		D2D1_COMBINE_MODE_INTERSECT = 1,
+
+		/// <summary>
+		/// Produce a geometry representing the set of points contained in the first
+		/// geometry or the second geometry, but not both.
+		/// </summary>
+		D2D1_COMBINE_MODE_XOR = 2,
+
+		/// <summary>
+		/// Produce a geometry representing the set of points contained in the first
+		/// geometry but not the second geometry.
+		/// </summary>
+		D2D1_COMBINE_MODE_EXCLUDE = 3,
+	}
 }
 
--- src/D2DLibExport/D2DLib.cs
+++ src/D2DLibExport/D2DLib.cs
@@ -26,15 +26,6 @@ using System.Runtime.InteropServices;
 
 namespace unvell.D2DLib
 {
-	public enum D2D1_COMBINE_MODE
-	{
-		D2D1_COMBINE_MODE_UNION = 0,
-		D2D1_COMBINE_MODE_INTERSECT = 1,
-		D2D1_COMBINE_MODE_XOR = 2,
-		D2D1_COMBINE_MODE_EXCLUDE = 3,
-		D2D1_COMBINE_MODE_FORCE_DWORD = -1
-	};
-
 	internal static class D2D
 	{
 
